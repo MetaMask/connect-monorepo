e0ca010c2c752816012449729657a1a64c3fd051
ffmcgee <joao.carlos@consensys.net>
2025-10-15 11:12:38 +0200

    build: migrate analytics package and build test

--- packages/analytics/src/index.ts
+++ packages/analytics/src/index.ts
@@ -1,1 +1,17 @@
-export { generateGreeting } from './greetings';
+/* eslint-disable no-restricted-globals */
+// eslint-disable-next-line @typescript-eslint/naming-convention
+import Analytics from './analytics';
+
+let endpoint: string | undefined;
+if (typeof process !== 'undefined' && process.env) {
+  endpoint =
+    process.env.METAMASK_ANALYTICS_ENDPOINT ??
+    process.env.NEXT_PUBLIC_METAMASK_ANALYTICS_ENDPOINT;
+}
+
+const METAMASK_ANALYTICS_ENDPOINT: string =
+  endpoint ?? 'https://mm-sdk-analytics.api.cx.metamask.io/';
+
+const client = new Analytics(METAMASK_ANALYTICS_ENDPOINT);
+
+export const analytics = client;
