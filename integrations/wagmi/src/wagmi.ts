3810ace6ea87382834665355ffb59837a222e37e
Alex Mendonca <alex.mendonca@consensys.net>
2025-11-06 17:10:45 +0000

    draft new metamask connector and port wagmi's playground

---
+++ integrations/wagmi/src/wagmi.ts
@@ -0,0 +1,21 @@
+import { createConfig, http } from 'wagmi';
+import { celo, mainnet, optimism, sepolia } from 'wagmi/chains';
+
+import { metaMask } from '../metamask-connector';
+
+export const config = createConfig({
+  chains: [mainnet, sepolia, optimism, celo],
+  connectors: [metaMask()],
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+    [optimism.id]: http(),
+    [celo.id]: http(),
+  },
+});
+
+declare module 'wagmi' {
+  interface Register {
+    config: typeof config;
+  }
+}
