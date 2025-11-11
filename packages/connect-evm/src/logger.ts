0b03494ed3926c9fe52a4dac849c22c0015aae97
Alex Mendonca <alex.mendonca@consensys.net>
2025-11-11 09:19:55 +0000

    fix typedoc on connect-evm

--- packages/connect-evm/src/logger.ts
+++ packages/connect-evm/src/logger.ts
@@ -3,6 +3,7 @@ import { createLogger, enableDebug } from '@metamask/connect-multichain';
 const namespace = 'metamask-connect:evm';
 
 // TODO: (@wenfix) Conditionally enable debug based on environment variable or storage setting
-enableDebug(namespace);
+// enableDebug(namespace);
 
+// @ts-expect-error logger needs to be typed properly
 export const logger = createLogger(namespace, '63');
